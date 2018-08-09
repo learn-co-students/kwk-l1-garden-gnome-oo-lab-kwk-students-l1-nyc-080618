8de0110f9c48ab80387630607dbc7acd705fea76
spethani <sarahpethani@gmail.com>
2018-08-09 20:01:20 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -52,3 +52,17 @@ class GardenGnome
     "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end
 end
+
+gnome1=GardenGnome.new
+
+gnome2=GardenGnome.new
+gnome2.name="Walter the Worst"
+
+gnome3=GardenGnome.new
+gnome3.name="James the Jerk"
+gnome3.age=3421
+
+gnome4=GardenGnome.new
+gnome4.name="Alfed the Abhorrent"
+gnome4.age=579
+gnome4.gluten_allergy=true
