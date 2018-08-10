5c518050b1d3961c563bb4a3cbe5467a7b8e832f
christinaalenda <clenda041@gmail.com>
2018-08-10 15:22:08 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,19 @@
 # Code your instances here
+class GardenGnome
+
+  def name 
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def age
+    @age
+  end
+  def age=(new_age)
+    @age = new_age
+  end
+  def personality=(new_personality)
+    @personality = new_personality
+  end
+end
